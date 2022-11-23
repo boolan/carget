9a3d8c8eb042106027bda2c985de2399b1e4001c
Krzysztof Bulanowski <krzysztof.bulanowski@capgemini.com>
2022-11-23 20:25:04 +0100

    Deployable SAPUI5 app, but not working on CF

---
+++ app/models/dist/Component-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("kbu.models.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
